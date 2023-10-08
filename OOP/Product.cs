e74bc6924c6d98a0c109713b6f69bf093a9c460a
gabrielfelipe <gabrielfelipeassuncaodesouza@gmail.com>
2023-10-08 15:03:02 -0300

    add first concepts of OOP in c#

---
+++ OOP/Product.cs
@@ -0,0 +1,6 @@
+class Product {
+  public string name { get; set; }
+  public float price { get; set; }
+
+  public void Discount() {}
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program {
+  static void Main(string[] args) {
+    Product p = new Product();
+    p.name = "gabriel";
+
+    Console.WriteLine(p.name);
+
+    var products = new List<Product>();
+    products.Add(new Product(){name="soap", price=1.0f});
+
+    products.ForEach(product => Console.WriteLine($"{product.name}\n{product.price}\n"));
+  }
+}
