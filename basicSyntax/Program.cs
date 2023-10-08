2a15fcfba1e6406086b96ee6e9fa4eb16a469d59
gabrielfelipe <gabrielfelipeassuncaodesouza@gmail.com>
2023-10-08 14:07:32 -0300

    add basic codes to learn the syntax

---
+++ basicSyntax/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int tam = 20;
+float euler = 2.78f;
+bool married = false;
+decimal fraction = 2.5m;
+
+Console.WriteLine($"Testing vars: {tam}\n{euler}\n{married}\n{fraction}");
+
+const decimal pi = 3.14m;
+Console.WriteLine($"{pi} is immutable");
+
+var text = "Hello, mom!";
+Console.WriteLine(text);
+
+//@ is equivalent to ''' in python
+//$ is equivalent to f in python
+
+Console.WriteLine(@"
+    Formatting a string
+
+    like this
+    ");
