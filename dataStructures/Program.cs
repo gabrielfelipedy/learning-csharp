f17ce0edf2ac43db694a1f372fec8a7f5270994f
gabrielfelipe <gabrielfelipeassuncaodesouza@gmail.com>
2023-10-08 14:35:44 -0300

    add new concepts about libs and arrays

---
+++ dataStructures/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] array = new int[4];
+
+for(int i = 0; i < array.Length; i++) {
+  array[i] = i+1;
+}
+
+for(int i = 0; i < array.Length; i++)
+  Console.WriteLine(array[i]);
+
+int[] array2 = new int[] {12, 13, 54, 12};
+
+for(int i = 0; i < array2.Length; i++)
+  Console.WriteLine(array2[i]);
+
+int[] array3 = {-1, -2, 3};
+
+for(int i = 0; i < array3.Length; i++)
+  Console.WriteLine(array3[i]);
---
+++ fnsAndLibs/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+static void printNum(int num) {
+  Console.WriteLine($"Num param: {num}");
+}
+
+printNum(10);
+Console.WriteLine(Math.Sqrt(256));
+
+Random r = new Random();
+
+Console.WriteLine(r.Next(1, 7));
+
+var stream = new StreamWriter("test.txt");
+stream.WriteLine("first line");
+
+stream.Close();
